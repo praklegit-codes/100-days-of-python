91ff84fbfa647c12c58fdb5ae152304764fe8aec
praklegit-codes <prakriti05.rai@gmail.com>
2026-09-06 16:58:37 +0530

    Create day02.py

---
+++ day02.py
@@ -0,0 +1,11 @@
+a=10
+print(a)
+b="harry"
+print(b)
+a1=100
+c=a+a1
+print(c)
+print("the type of a is",type(a))
+print("the type of a1 is",type(a1))
+print("the type of b is",type(b))
+print("the type of c is",type(c))
