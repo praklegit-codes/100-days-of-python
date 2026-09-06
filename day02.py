020fd71583c99fcf45d33c6b34f49f1ce5a50150
praklegit-codes <prakriti05.rai@gmail.com>
2026-09-06 17:38:29 +0530

    Update day02.py

--- day02.py
+++ day02.py
@@ -1,4 +1,4 @@
-a=10
+a=complex(8,9)
 print(a)
 b="harry"
 print(b)
@@ -9,3 +9,7 @@ print("the type of a is",type(a))
 print("the type of a1 is",type(a1))
 print("the type of b is",type(b))
 print("the type of c is",type(c))
+list=[1,2,3,4,[6,7,8,9],["apple"]]
+print(list)
+tuple=("Alice",25,"Female")
+print(tuple)
